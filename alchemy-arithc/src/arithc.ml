f0d028c22deb9402282d7356e38b9bdb41e482de
jorgebaptista <jorgebaptista.dev@outlook.com>
2025-12-30 21:32:19 +0000

    chore: update formatter configuration and format codebase

--- alchemy-arithc/src/arithc.ml
+++ alchemy-arithc/src/arithc.ml
@@ -1,4 +1,3 @@
-
 (* Ficheiro principal do compilador arithc *)
 
 open Format
@@ -10,15 +9,14 @@ let parse_only = ref false
 (* Nomes dos ficheiros 'source' e 'target' *)
 let ifile = ref ""
 let ofile = ref ""
-
 let set_file f s = f := s
 
 (* As opções do compilador para mostrar a mensagem iniical quando invocamos 'arithc --help' *)
 let options =
-  ["-parse-only", Arg.Set parse_only,
-   "  parsing stage only";
-   "-o", Arg.String (set_file ofile),
-   "<file>  output file name"]
+  [
+    ("-parse-only", Arg.Set parse_only, "  parsing stage only");
+    ("-o", Arg.String (set_file ofile), "<file>  output file name");
+  ]
 
 let usage = "usage: arithc [option] file.exp"
 
@@ -26,14 +24,17 @@ let usage = "usage: arithc [option] file.exp"
 let localisation pos =
   let l = pos.pos_lnum in
   let c = pos.pos_cnum - pos.pos_bol + 1 in
-  eprintf "File \"%s\", line %d, characters %d-%d:\n" !ifile l (c-1) c
+  eprintf "File \"%s\", line %d, characters %d-%d:\n" !ifile l (c - 1) c
 
 let () =
   (* Parsing da linha de comando *)
   Arg.parse options (set_file ifile) usage;
 
   (* Verifica-se que o nome do ficheiro de saída foi forneceido *)
-  if !ifile="" then begin eprintf "No file to compile!\n@?"; exit 1 end;
+  if !ifile = "" then begin
+    eprintf "No file to compile!\n@?";
+    exit 1
+  end;
 
   (* Este ficheiro tem de ter cono extensão .exp *)
   if not (Filename.check_suffix !ifile ".exp") then begin
@@ -44,7 +45,7 @@ let () =
 
   (* Por omissão, o ficheiro target partilha o nome do ficheiro fonte,
    exeptuando a extensão *)
-  if !ofile="" then ofile := Filename.chop_suffix !ifile ".exp" ^ ".s";
+  if !ofile = "" then ofile := Filename.chop_suffix !ifile ".exp" ^ ".s";
 
   (* Abertura do ficheiro em modo leitura *)
   let f = open_in !ifile in
@@ -64,28 +65,22 @@ let () =
     (* Paramos aqui se só pretendemos a fase de parsing *)
     if !parse_only then exit 0;
 
-     (* Compilamos a árvore de sintaxe abstrata p.
+    (* Compilamos a árvore de sintaxe abstrata p.
         O código máquina resultante de desta transformação deve ser
 	escrito no ficheiro target  ofile. *)
     Compile.compile_program p !ofile
   with
-    | Lexer.Lexing_error c ->
-	(* Erro léxico. Recuperamos a posição absoluta e convertêmo-la no formato linha-coluna *)
-	localisation (Lexing.lexeme_start_p buf);
-	eprintf "Lexical Error: %c@." c;
-	exit 1
-    | Parser.Error ->
-	(* Erro Sintático. Recuperamos a posição absoluta e convertêmo-la no formato linha-coluna *)
-	localisation (Lexing.lexeme_start_p buf);
-	eprintf "Syntax Error@.";
-	exit 1
-    | Compile.VarUndef s->
-	(* Erro derivado de um mau uso de variável durante a compilação *)
-	eprintf
-	  "Compilation Error: The variable %s is not definded@." s;
-	exit 1
-
-
-
-
-
+  | Lexer.Lexing_error c ->
+      (* Erro léxico. Recuperamos a posição absoluta e convertêmo-la no formato linha-coluna *)
+      localisation (Lexing.lexeme_start_p buf);
+      eprintf "Lexical Error: %c@." c;
+      exit 1
+  | Parser.Error ->
+      (* Erro Sintático. Recuperamos a posição absoluta e convertêmo-la no formato linha-coluna *)
+      localisation (Lexing.lexeme_start_p buf);
+      eprintf "Syntax Error@.";
+      exit 1
+  | Compile.VarUndef s ->
+      (* Erro derivado de um mau uso de variável durante a compilação *)
+      eprintf "Compilation Error: The variable %s is not definded@." s;
+      exit 1
--- alchemy-arithc/src/ast.ml
+++ alchemy-arithc/src/ast.ml
@@ -1,29 +1,37 @@
-
 (* Sintaxe abstracta para a linguagem Arith *)
 
 type program = stmt list
 
 and stmt =
-  | Set of string * expr       (* set x = e *)
-  | Print of expr              (* print e *)
-  | If of expr * stmt list * stmt list  (* if e then stmts else stmts *)
-  | While of expr * stmt list  (* while e do stmts done *)
+  | Set of string * expr (* set x = e *)
+  | Print of expr (* print e *)
+  | If of expr * stmt list * stmt list (* if e then stmts else stmts *)
+  | While of expr * stmt list (* while e do stmts done *)
 
 and expr =
-  | Cst of int                 (* integer constant *)
-  | Bool of bool               (* boolean constant: true, false *)
-  | Var of string              (* variable *)
-  | Binop of binop * expr * expr  (* binary operation *)
-  | Unop of unop * expr        (* unary operation *)
-  | Letin of string * expr * expr  (* let x = e1 in e2 *)
-  | IfExpr of expr * expr * expr   (* if e1 then e2 else e3 (expression) *)
+  | Cst of int (* integer constant *)
+  | Bool of bool (* boolean constant: true, false *)
+  | Var of string (* variable *)
+  | Binop of binop * expr * expr (* binary operation *)
+  | Unop of unop * expr (* unary operation *)
+  | Letin of string * expr * expr (* let x = e1 in e2 *)
+  | IfExpr of expr * expr * expr (* if e1 then e2 else e3 (expression) *)
 
 and binop =
-  | Add | Sub | Mul | Div      (* arithmetic *)
-  | Eq | Neq | Lt | Le | Gt | Ge  (* comparison *)
-  | And | Or                   (* logical *)
+  | Add
+  | Sub
+  | Mul
+  | Div (* arithmetic *)
+  | Eq
+  | Neq
+  | Lt
+  | Le
+  | Gt
+  | Ge (* comparison *)
+  | And
+  | Or (* logical *)
 
 and unop =
-  | Neg                        (* -e *)
-  | Not                        (* not e *)
-
+  | Neg
+  (* -e *)
+  | Not (* not e *)

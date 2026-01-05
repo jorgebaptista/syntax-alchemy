c363cea11a4ca3b85ec24c5e1c8be01779b3b994
Ricardoipr <55960821+Ricardoipr@users.noreply.github.com>
2026-01-05 22:16:13 +0000

    Update ast.ml

--- alchemy-arithc/src/ast.ml
+++ alchemy-arithc/src/ast.ml
@@ -1,18 +1,31 @@
 (* Sintaxe abstracta para a linguagem Arith *)
 
+(* Um programa é representado por:
+    - Uma lista de definições de funções no ambiente global (def list)
+    - Uma lista de instruções executadas sequencialmente com o corpo principal do programa (stmt list) 
+    Tem como objetivo facilitar a analise semântica e a geração de código nas diferentes fases do compilador*)  
 type program = def list * stmt list
+
+(* Uma definição de função é representada por:
+    - O nome da função (string)
+    - A lista de parâmetros formais (string list)
+    - O corpo da função, representado como uma lista de instruções (stmt list)*)
 and def = string * string list * stmt list
 
+(* Instruções (statements) da linguagem. Representam ações executáveis que afetam o estado do programa
+  e definem o controlo de fluxo durante a execução *)
 and stmt =
   | Set of string * expr (* set x = e *)
   | SetIndex of expr * expr * expr (* set a[i] = e *)
   | Print of expr (* print e *)
-  | Expr of expr (* evaluate expression, discard result *)
+  | Expr of expr (* avaliar expressão e descartar o resultado *)
   | If of expr * stmt list * stmt list (* if e then stmts else stmts *)
   | While of expr * stmt list (* while e do stmts done *)
   | For of string * expr * stmt list (* for x in e do stmts done *)
   | Return of expr (* return e *)
 
+(* Uma expressão (expr) representa uma construção que, quando avaliada,
+  produz um valor (inteiro, string, etc) *)
 and expr =
   | Cst of int (* integer constant *)
   | Bool of bool (* boolean constant: true, false *)
@@ -29,22 +42,28 @@ and expr =
   | Get of expr * expr (* e1[e2] *)
   | Len of expr (* len(e) *)
 
+(* Operadores binários *)  
 and binop =
+  (* Aritméticos *)
   | Add
   | Sub
   | Mul
-  | Div (* arithmetic *)
+  | Div 
   | Mod
+  (* Comparação *)
   | Eq
   | Neq
   | Lt
   | Le
   | Gt
-  | Ge (* comparison *)
+  | Ge
+  (* Lógicos *)
   | And
-  | Or (* logical *)
+  | Or
 
+(* Operadores unários *)  
 and unop =
-  | Neg
-  (* -e *)
+  (* Negação aritmética *)
+  | Neg (* -e *)
+  (* Negação lógica *)
   | Not (* not e *)
